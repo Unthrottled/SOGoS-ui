4c93e324df517aba458d2aad0d63efa67f1b84c1
DATBOI <alexsimons9999@gmail.com>
2019-05-01 04:59:43 -0500

    Can now decompose all this madness

--- src/sagas/SecuritySagas.js
+++ src/sagas/SecuritySagas.js
@@ -1,13 +1,16 @@
-import {takeEvery, take, put, fork, all, select} from 'redux-saga/effects'
+import {all, call, fork, put, select, take, takeEvery} from 'redux-saga/effects'
 import {INITIALIZED_APPLICATION} from "../actions/ApplicationLifecycleActions";
 import {RECEIVED_OAUTH_CONFIGURATION, requestOAuthConfigurations} from "../actions/ConfigurationActions";
 import {needsToLogIn, needsToRefreshToken} from "../security/OAuth";
 import {
   AuthorizationNotifier,
-  AuthorizationRequest, BaseTokenRequestHandler, GRANT_TYPE_AUTHORIZATION_CODE,
-  RedirectRequestHandler, TokenRequest
+  AuthorizationRequest,
+  BaseTokenRequestHandler,
+  GRANT_TYPE_AUTHORIZATION_CODE,
+  RedirectRequestHandler,
+  TokenRequest
 } from "@openid/appauth";
-import { NodeCrypto } from '@openid/appauth/built/node_support/';
+import {NodeCrypto} from '@openid/appauth/built/node_support/';
 
 function* securityRequestSaga() {
   // Make sure that credentials are up to date
@@ -17,52 +20,65 @@ function* securityRequestSaga() {
   yield oauthInitializationSaga(oauthConfig);
 }
 
-function* oauthInitializationSaga(oauthConfig){
+function* listenToRedirect(notifier, oauthConfig) {
+  try {
+    const {request, response} = yield call(() => {
+      return new Promise(((resolve, reject) => {
+        notifier.setAuthorizationListener((request, response, error) => {
+          if (error || !request) {
+            reject(error)
+          } else {
+            resolve({
+              request,
+              response
+            })
+          }
+        });
+      }));
+    });
+    const code = response.code;
+    const codeVerifier = request.internal && request.internal.code_verifier;
+    const tokenRequest = new TokenRequest({
+      client_id: "sogos-app",
+      redirect_uri: 'http://localhost:3000',
+      grant_type: GRANT_TYPE_AUTHORIZATION_CODE,
+      code,
+      extras: {
+        code_verifier: codeVerifier
+      }
+    });
+    const tokenResponse = yield call(()=>new BaseTokenRequestHandler().performTokenRequest(oauthConfig, tokenRequest));
+    console.log(tokenResponse)
+  } catch (e) {
+  }
+}
+
+function* oauthInitializationSaga(oauthConfig) {
   const {securityState} = yield select();
-  if(needsToLogIn(securityState)){
+  if (needsToLogIn(securityState)) {
     const notifier = new AuthorizationNotifier();
     const authorizationHandler = new RedirectRequestHandler();
     authorizationHandler.setAuthorizationNotifier(notifier);
-    notifier.setAuthorizationListener((request, response, error)=>{
-      console.log('Authorization request complete ', request, response, error);
-      if (response) {
-        const code = response.code;
-        const codeVerifier = request.internal && request.internal.code_verifier;
-        const tokenRequest = new TokenRequest({
-          client_id: "sogos-app",
-          redirect_uri: 'http://localhost:3000',
-          grant_type: GRANT_TYPE_AUTHORIZATION_CODE,
-          code,
-          refresh_token: undefined,
-          extras: {
-            code_verifier : codeVerifier
-          }
-        });
-        new BaseTokenRequestHandler().performTokenRequest(oauthConfig, tokenRequest)
-          .then(response =>{
-            console.log(`Got this shit`, response);
-          })
-      }
-    });
-    authorizationHandler.completeAuthorizationRequestIfPossible()
-      .then(() => {
-        if(window.location.search.indexOf('state') < 0){
-          const scope = 'openid profile email';
-          const authorizationRequest = new AuthorizationRequest({
-            client_id: 'sogos-app',
-            redirect_uri: 'http://localhost:3000',
-            scope,
-            response_type: AuthorizationRequest.RESPONSE_TYPE_CODE,
-          }, new NodeCrypto());
-          authorizationHandler.performAuthorizationRequest(oauthConfig, authorizationRequest);
-        }
-      })
-  } else if(needsToRefreshToken(securityState)){
+    yield fork(listenToRedirect, notifier, oauthConfig);
+    yield call(()=>authorizationHandler.completeAuthorizationRequestIfPossible());
+    if (window.location.search.indexOf('state') < 0) {
+      const scope = 'openid profile email';
+      const authorizationRequest = new AuthorizationRequest({
+        client_id: 'sogos-app',
+        redirect_uri: 'http://localhost:3000',
+        scope,
+        response_type: AuthorizationRequest.RESPONSE_TYPE_CODE,
+      }, new NodeCrypto());
+      authorizationHandler.performAuthorizationRequest(oauthConfig, authorizationRequest);
+    } else {
+      //clean up route
+    }
+  } else if (needsToRefreshToken(securityState)) {
 
   }
 }
 
-function* listenToStartupEvent(){
+function* listenToStartupEvent() {
   yield takeEvery(INITIALIZED_APPLICATION, securityRequestSaga)
 }
 
@@ -70,7 +86,7 @@ function* listenToAppLifecycleEvents() {
   yield fork(listenToStartupEvent);
 }
 
-export default function* rootSaga(){
+export default function* rootSaga() {
   yield all([
     listenToAppLifecycleEvents()
   ])
