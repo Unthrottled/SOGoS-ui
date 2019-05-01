0399956159b5ea2fd9829cf75feb14be1e695585
DATBOI <alexsimons9999@gmail.com>
2019-05-01 06:06:30 -0500

    More finishing saga with logged in user

--- src/sagas/SecuritySagas.js
+++ src/sagas/SecuritySagas.js
@@ -2,11 +2,13 @@ import {all, fork, put, take, takeEvery} from 'redux-saga/effects'
 import {INITIALIZED_APPLICATION} from "../actions/ApplicationLifecycleActions";
 import {RECEIVED_OAUTH_CONFIGURATION, requestOAuthConfigurations} from "../actions/ConfigurationActions";
 import oauthInitializationSaga from './security/SecurityInitializationSaga';
+import {createLoggedOnAction} from "../actions/SecurityActions";
 
 function* securityRequestSaga() {
   yield put(requestOAuthConfigurations()); // ask for oauth configurations
   const {payload: oauthConfig} = yield take(RECEIVED_OAUTH_CONFIGURATION); // yay configurations!
   yield oauthInitializationSaga(oauthConfig);// Log user in or refresh tokens
+  yield put(createLoggedOnAction());
 }
 
 function* listenToStartupEvent() {
