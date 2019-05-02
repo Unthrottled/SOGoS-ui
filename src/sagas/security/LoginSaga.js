10a908242ed9259348767032cf5f03e0dfc4a0d4
DATBOI <alexsimons9999@gmail.com>
2019-05-02 05:47:34 -0500

    Get your shit together, get it all together and put it in a back pack, all your shit, so it's together. And if you gotta take it some where, take it somewhere, you know, take it to the shit store and sell it, or put it in the shit museum. I don't care what you do, you just gotta get it together. Get your shit together.

--- src/sagas/security/LoginSaga.js
+++ src/sagas/security/LoginSaga.js
@@ -1,90 +1,17 @@
 import {
-  AuthorizationError,
   AuthorizationNotifier,
   AuthorizationRequest,
-  AuthorizationRequestResponse, AuthorizationResponse, BasicQueryStringUtils, GRANT_TYPE_AUTHORIZATION_CODE,
-  RedirectRequestHandler, TokenRequest
+  AuthorizationRequestResponse,
+  GRANT_TYPE_AUTHORIZATION_CODE,
+  RedirectRequestHandler,
+  TokenRequest
 } from "@openid/appauth";
 import {NodeCrypto} from '@openid/appauth/built/node_support/';
 import {createLoggedOnAction} from "../../actions/SecurityActions";
 import {call, put} from 'redux-saga/effects'
 import {getNewTokens} from "./SecurityInitializationSaga";
+import {completeAuthorizationRequest} from "../../security/StupidShit";
 
-const AUTHORIZATION_REQUEST_HANDLE_KEY = 'appauth_current_authorization_request';
-
-const authorizationServiceConfigurationKey =
-  (handle: string) => `${handle}_appauth_authorization_service_configuration`;
-
-const authorizationRequestKey =
-  (handle: string) => `${handle}_appauth_authorization_request`;
-
-const queryStringBitch = new BasicQueryStringUtils();
-
-const getResponse = (error, queryParams): AuthorizationResponse =>{
-  if(error){
-    return null
-  } else {
-    return new AuthorizationResponse({
-      code: queryParams['code'],
-      state: queryParams['state']
-    })
-  }
-};
-
-const getError = (error, queryParams): AuthorizationError =>{
-  if(error){
-    return new AuthorizationError({
-      error: error,
-      error_description: queryParams['error_description'],
-      error_uri: queryParams['error_uri'],
-      state: queryParams['state'],
-    })
-  } else {
-    return null;
-  }
-};
-
-const cleanUpURI = ()=>{
-  const uri = window.location.toString();
-  const queryParamStart = uri.indexOf("?");
-  if(queryParamStart){
-    const clean_uri = uri.substring(0, queryParamStart);
-    console.log(clean_uri)
-    window.history.replaceState({}, document.title, clean_uri);
-  }
-};
-
-const completeRequest = (storageBackend, handle) =>
-  storageBackend.getItem(authorizationRequestKey(handle))
-    .then(authorizationRequest => JSON.parse(authorizationRequest))
-    .then(authorizationRequest => new AuthorizationRequest(authorizationRequest))
-    .then(authorizationRequest => {
-      const location = window.location;
-      const queryParams = queryStringBitch.parse(location, false);//all that work to change this stupid line......Thanks Obama.
-      const error: string|undefined = queryParams['error'];
-      const shouldNotify = queryParams['state'] === authorizationRequest.state;
-      if(shouldNotify){
-        return Promise.all([
-            storageBackend.removeItem(AUTHORIZATION_REQUEST_HANDLE_KEY),
-            storageBackend.removeItem(authorizationRequestKey(handle)),
-            storageBackend.removeItem(authorizationServiceConfigurationKey(handle))
-          ]).then(()=>({
-          request: authorizationRequest,
-          response: getResponse(error, queryParams),
-          error: getError(error, queryParams),
-        }))
-      } else {
-        return Promise.resolve(null);
-      }
-    });
-
-const completeAuthorizationRequest= (authorizationHandler: RedirectRequestHandler) =>
-  authorizationHandler.storageBackend.getItem(AUTHORIZATION_REQUEST_HANDLE_KEY)
-    .then(handle => handle ? completeRequest(authorizationHandler.storageBackend, handle): null)
-    .then(result =>{
-      cleanUpURI();
-      return result;
-    });
 
 function* loginSaga({payload: oauthConfig}) {
   const notifier = new AuthorizationNotifier();
@@ -106,7 +33,6 @@ function* loginSaga({payload: oauthConfig}) {
     yield getThatTokenYo(request, response, oauthConfig);
     yield put(createLoggedOnAction());
   }
-
 }
 
 function* getThatTokenYo(request, response, oauthConfig) {
