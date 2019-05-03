f3ffa751d8005c113b2b524df717af4d5950f441
DATBOI <alexsimons9999@gmail.com>
2019-05-03 05:08:25 -0500

    Got rid of some things in the user events

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,8 +1,6 @@
-import axios from "axios";
 
 export const FAILED_REQUESTED_USER: 'FAILED_REQUESTED_USER' = 'FAILED_REQUESTED_USER';
 export const RECEIVED_USER: 'RECEIVED_USER' = 'RECEIVED_USER';
-export const REQUESTED_USER: 'REQUESTED_USER' = 'REQUESTED_USER';
 
 export type User = {
   fullName: string,
@@ -11,11 +9,6 @@ export type User = {
   lastName: string,
   email: string,
 }
-
-
-export const requestUser = () => ({
-  type: REQUESTED_USER,
-});
 
 export const receivedUser = (user: User) => ({
   type: RECEIVED_USER,
@@ -26,23 +19,3 @@ export const failedToGetUser = (error) => ({
   type: FAILED_REQUESTED_USER,
   payload: error
 });
-
-const fetchUser = () => dispetch => {
-  dispetch(requestUser());
-  return axios.get('./api/user')
-    .then(response => response.data)
-    .then(user => dispetch(receivedUser(user)))
-    .catch(error => dispetch(failedToGetUser(error)))
-
-};
-
-const shouldFindWaldo = (state) => {
-  return !state.user.information.email; // todo: maybe have a function that gets stuff out of the state
-};
-
-export const wheresWaldo = () => (dispetch, getState) =>{
-  if(shouldFindWaldo(getState())){
-    return dispetch(fetchUser())
-  }
-};
-
