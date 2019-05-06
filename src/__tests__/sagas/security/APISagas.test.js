3d8a3d7fdd1ad86d8298194ac259fe95a0046786
DATBOI <alexsimons9999@gmail.com>
2019-05-06 06:21:44 -0500

    Added options to test

--- src/__tests__/sagas/security/APISagas.test.js
+++ src/__tests__/sagas/security/APISagas.test.js
@@ -11,62 +11,140 @@ import axios from 'axios';
 describe('API Sagas', () => {
 
   describe('performGet', () => {
-    const it = sagaHelper(performGet('http://localhost/api/onions'));
-    it('should put in a request for a access token', (result) => {
-      expect(result).toEqual(put(createRequestAccessTokenEvent()))
+    describe('when not supplied options', () => {
+      const it = sagaHelper(performGet('http://localhost/api/onions'));
+      it('should put in a request for a access token', (result) => {
+        expect(result).toEqual(put(createRequestAccessTokenEvent()))
+      });
+      it('should then listen for a response', (result) => {
+        expect(result).toEqual(take(FOUND_ACCESS_TOKEN));
+        return createFoundAccessTokenEvent('I AM ACCESS TOKEN, YO');
+      });
+      it('should then perform a get request with authentication', (result) => {
+        expect(result).toEqual(call(axios.get,
+          'http://localhost/api/onions', {
+            headers:{
+              Authorization: 'Bearer I AM ACCESS TOKEN, YO'
+            },
+          }));
+        return 'One response from the backend'
+      });
+      it('should then return the response of the backend call', (result) => {
+        expect(result).toEqual('One response from the backend');
+        return 'I am done!'
+      });
+      it('should complete', (result) => {
+        expect(result).toBeUndefined();
+      });
     });
-    it('should then listen for a response', (result) => {
-      expect(result).toEqual(take(FOUND_ACCESS_TOKEN));
-      return createFoundAccessTokenEvent('I AM ACCESS TOKEN, YO');
-    });
-    it('should then perform a get request with authentication', (result) => {
-      expect(result).toEqual(call(axios.get,
-        'http://localhost/api/onions', {
-          headers:{
-            Authorization: 'Bearer I AM ACCESS TOKEN, YO'
-          },
-        }));
-      return 'One response from the backend'
-    });
-    it('should then return the response of the backend call', (result) => {
-      expect(result).toEqual('One response from the backend');
-      return 'I am done!'
-    });
-    it('should complete', (result) => {
-      expect(result).toBeUndefined();
+    describe('when supplied options', () => {
+      const it = sagaHelper(performGet('http://localhost/api/onions', {
+        slip: 'knot',
+        headers: {
+          'Authorization': 'Basic Bitch',
+          'X-QUICK-SCOPE': 'L337'
+        }
+      }));
+      it('should put in a request for a access token', (result) => {
+        expect(result).toEqual(put(createRequestAccessTokenEvent()))
+      });
+      it('should then listen for a response', (result) => {
+        expect(result).toEqual(take(FOUND_ACCESS_TOKEN));
+        return createFoundAccessTokenEvent('I AM ACCESS TOKEN, YO');
+      });
+      it('should then perform a get request with authentication', (result) => {
+        expect(result).toEqual(call(axios.get,
+          'http://localhost/api/onions', {
+            slip: 'knot',
+            headers:{
+              Authorization: 'Bearer I AM ACCESS TOKEN, YO',
+              'X-QUICK-SCOPE': 'L337',
+            },
+          }));
+        return 'One response from the backend'
+      });
+      it('should then return the response of the backend call', (result) => {
+        expect(result).toEqual('One response from the backend');
+        return 'I am done!'
+      });
+      it('should complete', (result) => {
+        expect(result).toBeUndefined();
+      });
     });
   });
 
   describe('performPost', () => {
-    const it = sagaHelper(performPost('http://localhost/api/onions', {
-      'I AM': 'BECOME DEATH',
-    }));
-    it('should put in a request for a access token', (result) => {
-      expect(result).toEqual(put(createRequestAccessTokenEvent()))
-    });
-    it('should then listen for a response', (result) => {
-      expect(result).toEqual(take(FOUND_ACCESS_TOKEN));
-      return createFoundAccessTokenEvent('I AM ACCESS TOKEN, YO');
-    });
-    it('should then perform a get request with authentication', (result) => {
-      expect(result).toEqual(call(axios.post,
-        'http://localhost/api/onions',
-        {
-          'I AM': 'BECOME DEATH',
-        },
-        {
-          headers:{
-            Authorization: 'Bearer I AM ACCESS TOKEN, YO',
+    describe('when not supplied options', () => {
+      const it = sagaHelper(performPost('http://localhost/api/onions', {
+        'I AM': 'BECOME DEATH',
+      }));
+      it('should put in a request for a access token', (result) => {
+        expect(result).toEqual(put(createRequestAccessTokenEvent()))
+      });
+      it('should then listen for a response', (result) => {
+        expect(result).toEqual(take(FOUND_ACCESS_TOKEN));
+        return createFoundAccessTokenEvent('I AM ACCESS TOKEN, YO');
+      });
+      it('should then perform a get request with authentication', (result) => {
+        expect(result).toEqual(call(axios.post,
+          'http://localhost/api/onions',
+          {
+            'I AM': 'BECOME DEATH',
           },
-        }));
-      return 'One response from the backend'
+          {
+            headers:{
+              Authorization: 'Bearer I AM ACCESS TOKEN, YO',
+            },
+          }));
+        return 'One response from the backend'
+      });
+      it('should then return the response of the backend call', (result) => {
+        expect(result).toEqual('One response from the backend');
+        return 'I am done!'
+      });
+      it('should complete', (result) => {
+        expect(result).toBeUndefined();
+      });
     });
-    it('should then return the response of the backend call', (result) => {
-      expect(result).toEqual('One response from the backend');
-      return 'I am done!'
-    });
-    it('should complete', (result) => {
-      expect(result).toBeUndefined();
+    describe('when supplied options', () => {
+      const it = sagaHelper(performPost('http://localhost/api/onions', {
+        'I AM': 'BECOME DEATH',
+      }, {
+        standby: 'for titan fall',
+        headers: {
+          'Authorization': 'Basic Bitch',
+          'X-QUICK-SCOPE': 'L337'
+        }
+      }));
+      it('should put in a request for a access token', (result) => {
+        expect(result).toEqual(put(createRequestAccessTokenEvent()))
+      });
+      it('should then listen for a response', (result) => {
+        expect(result).toEqual(take(FOUND_ACCESS_TOKEN));
+        return createFoundAccessTokenEvent('I AM ACCESS TOKEN, YO');
+      });
+      it('should then perform a get request with authentication', (result) => {
+        expect(result).toEqual(call(axios.post,
+          'http://localhost/api/onions',
+          {
+            'I AM': 'BECOME DEATH',
+          },
+          {
+            standby: 'for titan fall',
+            headers:{
+              Authorization: 'Bearer I AM ACCESS TOKEN, YO',
+              'X-QUICK-SCOPE': 'L337',
+            },
+          }));
+        return 'One response from the backend'
+      });
+      it('should then return the response of the backend call', (result) => {
+        expect(result).toEqual('One response from the backend');
+        return 'I am done!'
+      });
+      it('should complete', (result) => {
+        expect(result).toBeUndefined();
+      });
     });
   });
 });
