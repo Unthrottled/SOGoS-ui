617d9cd83ad5344d2c388695f5a61ff64d167c70
DATBOI <alexsimons9999@gmail.com>
2019-05-07 07:12:09 -0500

    Added API documentation for token saga

--- src/sagas/security/TokenSagas.js
+++ src/sagas/security/TokenSagas.js
@@ -4,7 +4,15 @@ import {call, put} from 'redux-saga/effects'
 
 const tokenHandler: TokenRequestHandler = new BaseTokenRequestHandler();
 
+/**
+ * Attempts to fetch token from Authorization Server.
+ * @param oauthConfig
+ * @param tokenRequest
+ * @returns
+ * @throws Runtime Exception Be wary (for now)
+ */
 export function* fetchTokenSaga(oauthConfig, tokenRequest) {
-  const tokenResponse = yield call(() => tokenHandler.performTokenRequest(oauthConfig, tokenRequest));
+  // todo: refracture to catch exception and dispatch token failure event
+  const tokenResponse = yield call(tokenHandler.performTokenRequest, oauthConfig, tokenRequest);
   yield put(createTokenReceptionEvent(tokenResponse));
 }
