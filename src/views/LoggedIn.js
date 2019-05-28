235a0f5cbea8d8f4fe84f0fa581b21bc3161a7fd
DATBOI <alexsimons9999@gmail.com>
2019-05-28 06:18:13 -0500

    Added svgs, guess I am going to have to learn hooks

--- src/views/LoggedIn.js
+++ src/views/LoggedIn.js
@@ -2,6 +2,18 @@ import React from "react";
 import {connect} from "react-redux";
 import {logout} from "../actions/SecurityActions";
 import {startActivity} from "../actions/ActivityActions";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from '@material-ui/icons/Add';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  fab: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 class LoggedIn extends React.Component {
   constructor(props) {
@@ -23,9 +35,13 @@ class LoggedIn extends React.Component {
 
   render() {
     const {fullName} = this.props;
+    const classes={}
     return (
       <div>
         <h3>What's up {fullName}?</h3>
+        <Fab onClick={()=>console.log('Finna bust a nut')} color="primary" aria-label="Add" className={classes.fab}>
+          <AddIcon />
+        </Fab>
         <button onClick={()=>this.startActivity()}>Start Activity</button>
         There's a ninja with huge boobs over there.
         <button onClick={() => this.logout()}>Logout</button>
