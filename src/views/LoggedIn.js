5af585a8866640ed567081f8af8616d115acf1b9
DATBOI <alexsimons9999@gmail.com>
2019-05-29 05:27:03 -0500

    I've got the need, the need for speed

--- src/views/LoggedIn.js
+++ src/views/LoggedIn.js
@@ -2,23 +2,31 @@ import React from "react";
 import {connect} from "react-redux";
 import {logout} from "../actions/SecurityActions";
 import {startActivity} from "../actions/ActivityActions";
-import Fab from "@material-ui/core/Fab";
-import AddIcon from '@material-ui/icons/Add';
-import {makeStyles} from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
+import SpeedDial from "@material-ui/lab/SpeedDial";
+import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
+import SpeedDialAction from "@material-ui/lab/SpeedDialAction";
+import FileCopyIcon from '@material-ui/icons/FileCopyOutlined';
+import SaveIcon from '@material-ui/icons/Save';
+import PrintIcon from '@material-ui/icons/Print';
+import ShareIcon from '@material-ui/icons/Share';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-const useStyles = makeStyles(theme => ({
-  fab: {
-    margin: theme.spacing(1),
-  },
-  extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
-}));
+
+const actions = [
+  {icon: <FileCopyIcon/>, name: 'Copy'},
+  {icon: <SaveIcon/>, name: 'Save'},
+  {icon: <PrintIcon/>, name: 'Print'},
+  {icon: <ShareIcon/>, name: 'Share'},
+  {icon: <DeleteIcon/>, name: 'Delete'},
+];
 
 class LoggedIn extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      open: false
+    };
   }
 
   startActivity(): void {
@@ -33,16 +41,47 @@ class LoggedIn extends React.Component {
     dispetch(logout());
   }
 
+  handleClick = () => {
+    this.setState(state => ({
+      open: !state.open,
+    }));
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
   render() {
-    const {fullName} = this.props;
-    const classes={}
+    const {fullName, classes} = this.props;
+    const {open} = this.state;
+
     return (
       <div>
         <h3>What's up {fullName}?</h3>
-        <Fab onClick={()=>console.log('Finna bust a nut')} color="primary" aria-label="Add" className={classes.fab}>
-          <AddIcon />
-        </Fab>
-        <button onClick={()=>this.startActivity()}>Start Activity</button>
+        <SpeedDial
+          ariaLabel="SpeedDial example"
+          className={classes.speedDial}
+          hidden={false}
+          icon={<SpeedDialIcon/>}
+          onClick={this.handleClick}
+          onClose={this.handleClose}
+          open={open}
+        >
+          {actions.map(action => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={action.name}
+              onClick={this.handleClick}
+              title={""}
+              children={<div/>}/>
+          ))}
+        </SpeedDial>
+        <button onClick={() => this.startActivity()}>Start Activity</button>
         There's a ninja with huge boobs over there.
         <button onClick={() => this.logout()}>Logout</button>
       </div>
@@ -59,4 +98,15 @@ const mapStateToProps = state => {
   }
 };
 
-export default connect(mapStateToProps)(LoggedIn);
+const styles = theme => ({
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  speedDial: {
+    position: 'absolute',
+    top: theme.spacing(1),
+    left: theme.spacing(1),
+    margin: theme.spacing(1),
+  },
+});
+export default connect(mapStateToProps)(withStyles(styles)(LoggedIn));
