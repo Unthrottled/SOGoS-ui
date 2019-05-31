86ca6f025121638b8999264ef4b50e6d7f2a0d13
DATBOI <alexsimons9999@gmail.com>
2019-05-31 05:21:26 -0500

    I like functional components :)

--- src/views/LoggedOut.js
+++ src/views/LoggedOut.js
@@ -2,31 +2,16 @@ import React from "react";
 import {connect} from "react-redux";
 import {login} from "../actions/SecurityActions";
 
-class LoggedOut extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  login(): void {
-    const {dispatch: dispetch} = this.props;
+function LoggedOut({dispatch: dispetch}) {
+  const logUserIn = (): void => {
     dispetch(login());
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Hey it looks like you need to login!</h3>
-        <button onClick={() => this.login()}>Login</button>
-      </div>
-    );
-  }
+  };
+  return (
+    <div>
+      <h3>Hey it looks like you need to login!</h3>
+      <button onClick={() => logUserIn()}>Login</button>
+    </div>
+  );
 }
 
-LoggedOut.propTypes = {};
-
-const mapStateToProps = state => {
-  return {}
-};
-
-export default connect(mapStateToProps)(LoggedOut);
+export default connect()(LoggedOut);
