98eab2d08f4be98f1bd9a1ac5f1337ad287f55b1
DATBOI <alexsimons9999@gmail.com>
2019-05-31 05:10:32 -0500

    More activity component

---
+++ src/views/Activity.js
@@ -0,0 +1,68 @@
+import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
+import SpeedDialAction from "@material-ui/lab/SpeedDialAction";
+import SpeedDial from "@material-ui/lab/SpeedDial";
+import React, {useState} from "react";
+import Timer from '@material-ui/icons/AvTimer';
+import {makeStyles} from '@material-ui/core/styles';
+import uuid from 'uuid/v4';
+import {startActivity} from "../actions/ActivityActions";
+import {connect} from "react-redux";
+
+const useStyles = makeStyles(theme => ({
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  speedDial: {
+    position: 'absolute',
+    top: theme.spacing(1),
+    left: theme.spacing(1),
+    margin: theme.spacing(1),
+  },
+}));
+
+
+const Activity = ({dispatch: dispetch}) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const commenceActivity = () => {
+    dispetch(startActivity({
+      name: "SOME_ACTIVITY",
+      uuid: uuid(),
+    }));
+  };
+  const handleClick = () => {
+    setOpen(!open)
+  };
+
+  const actions = [
+    {icon: <Timer/>, name: 'Start Task', perform: () => commenceActivity()},
+  ];
+
+  return (
+    <SpeedDial
+      ariaLabel="SpeedDial example"
+      className={classes.speedDial}
+      hidden={false}
+      icon={<SpeedDialIcon/>}
+      onClick={handleClick}
+      open={open}
+      direction={"right"}
+    >
+      {actions.map(action => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={() => {
+            handleClick();
+            action.perform();
+          }}
+          title={""}
+          children={<div/>}/>
+      ))}
+    </SpeedDial>
+  );
+};
+
+export default connect()(Activity);
--- src/views/LoggedIn.js
+++ src/views/LoggedIn.js
@@ -1,15 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import {logout} from "../actions/SecurityActions";
-import {startActivity} from "../actions/ActivityActions";
-import {withStyles} from "@material-ui/core/styles";
-import SpeedDial from "@material-ui/lab/SpeedDial";
-import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
-import SpeedDialAction from "@material-ui/lab/SpeedDialAction";
-import Timer from '@material-ui/icons/AvTimer';
-import uuid from 'uuid/v4';
-
-
+import Activity from "./Activity";
 
 class LoggedIn extends React.Component {
   constructor(props) {
@@ -19,64 +11,17 @@ class LoggedIn extends React.Component {
     };
   }
 
-  actions = [
-    {icon: <Timer/>, name: 'Start Task', perform: ()=>this.startActivity()},
-  ];
-
-  startActivity(): void {
-    const {dispatch: dispetch} = this.props;
-    dispetch(startActivity({
-      name: "SOME_ACTIVITY",
-      uuid: uuid(),
-    }));
-  }
-
   logout(): void {
     const {dispatch: dispetch} = this.props;
     dispetch(logout());
   }
 
-  handleClick = () => {
-    this.setState(state => ({
-      open: !state.open,
-    }));
-  };
-
-  handleClose = () => {
-    this.setState({open: false});
-  };
-
   render() {
-    const {fullName, classes} = this.props;
-    const {open} = this.state;
-    const {actions} = this;
-
+    const {fullName} = this.props;
     return (
       <div>
+        <Activity/>
         <h3>What's up {fullName}?</h3>
-        <SpeedDial
-          ariaLabel="SpeedDial example"
-          className={classes.speedDial}
-          hidden={false}
-          icon={<SpeedDialIcon/>}
-          onClick={this.handleClick}
-          onClose={this.handleClose}
-          open={open}
-          direction={"right"}
-        >
-          {actions.map(action => (
-            <SpeedDialAction
-              key={action.name}
-              icon={action.icon}
-              tooltipTitle={action.name}
-              onClick={()=>{
-                this.handleClick();
-                action.perform();
-              }}
-              title={""}
-              children={<div/>}/>
-          ))}
-        </SpeedDial>
         There's a ninja with huge boobs over there.
         <button onClick={() => this.logout()}>Logout</button>
       </div>
@@ -93,15 +38,4 @@ const mapStateToProps = state => {
   }
 };
 
-const styles = theme => ({
-  extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
-  speedDial: {
-    position: 'absolute',
-    top: theme.spacing(1),
-    left: theme.spacing(1),
-    margin: theme.spacing(1),
-  },
-});
-export default connect(mapStateToProps)(withStyles(styles)(LoggedIn));
+export default connect(mapStateToProps)(LoggedIn);
