cfb37db61efb0faaae2dc5a380df976fc310b25c
DATBOI <alexsimons9999@gmail.com>
2019-06-03 06:26:02 -0500

    Added stop button, just need to make it work and look pretty

--- src/views/ActivityTimer.js
+++ src/views/ActivityTimer.js
@@ -4,33 +4,41 @@ import {connect} from "react-redux";
 import Slide from "@material-ui/core/Slide";
 import Timer from "./Timer";
 import Close from '@material-ui/icons/Close';
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 
 const useStyles = makeStyles(theme => ({
   timer: {
     position: 'fixed',
     bottom: 0,
-    textAlign: 'center',
     width: '100%',
     display: 'flex',
     paddingTop: theme.spacing(0.5),
     paddingBottom: theme.spacing(0.5),
     background: theme.palette.secondary.main
   },
-  close: {}
+  close: {
+    display: 'inline-flex',
+    position: 'relative',
+    cursor: 'pointer',
+    paddingRight: theme.spacing(1)
+  },
 }));
 
 const getTime = antecedenceTime => Math.floor((new Date().getTime() - antecedenceTime) / 1000);
 
-const ActivityTimer = ({shouldTime, antecedenceTime}) => {
+const ActivityTimer = ({shouldTime, antecedenceTime, dispatch: dispetch}) => {
   const classes = useStyles();
+  const stopActivity = () =>{
+    console.log('finna bust a nut');
+  };
   return shouldTime ? (
     <Slide direction={"up"} in={shouldTime}>
       <div className={classes.timer}>
-        <div style={{flexGrow: 1}}>
+        <div style={{flexGrow: 1, textAlign: "center"}}>
           <Timer startTimeInSeconds={getTime(antecedenceTime)}/>
         </div>
-        <div>
-          <Close className={classes.close}/>
+        <div onClick={stopActivity} className={classes.close}>
+          <Close/>
         </div>
       </div>
     </Slide>) : null;
