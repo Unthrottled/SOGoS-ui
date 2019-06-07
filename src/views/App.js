333c3699fb4d769f0b96615371b78ff96607c5fd
DATBOI <alexsimons9999@gmail.com>
2019-06-07 04:44:58 -0500

    Updated the manifest

--- src/views/App.js
+++ src/views/App.js
@@ -7,11 +7,11 @@ import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
 import {ThemeProvider} from '@material-ui/styles'
 import {createMuiTheme, responsiveFontSizes} from '@material-ui/core/styles';
-import {green, teal} from "@material-ui/core/colors";
+import {green, cyan} from "@material-ui/core/colors";
 
 const theme = responsiveFontSizes(createMuiTheme({
   palette: {
-    primary: teal,
+    primary: cyan,
     secondary: green,
   }
 }));
