0e33de9cd26286c64d678a8486710640e326cdc5
DATBOI <alexsimons9999@gmail.com>
2019-06-07 05:41:32 -0500

    Added some service worker stuffs

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {fetchApplicationConfiguration} from "./config/Configuration";
 import {PersistGate} from "redux-persist/lib/integration/react";
+import initWorkboxRefresh from '@loopmode/cra-workbox-refresh';
 
 const { store, persistor } = fetchApplicationConfiguration();
 
@@ -18,7 +19,24 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const renderRefreshUI = registration =>{
+  const refreshButton = document.createElement('button');
+  refreshButton.textContent = 'Update Yo';
+  refreshButton.className = 'update-button';
+  refreshButton.addEventListener('click',()=>{
+    refreshButton.disabled = true;
+    registration.waiting.postMessage('things?')
+  });
+
+  document.getElementById('root').appendChild(refreshButton)
+};
+
+if(process.env.NODE_ENV === 'production'){
+  serviceWorker.register({
+    onUpdate: registration => initWorkboxRefresh(registration, {
+      render: renderRefreshUI
+    })
+  });
+} else {
+  serviceWorker.unregister();
+}
