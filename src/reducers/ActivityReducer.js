21b0982a7dc405a5ca41dab2929d7049d9137651
DATBOI <alexsimons9999@gmail.com>
2019-06-10 11:02:28 -0500

    More clearing the activity cache on sync

--- src/reducers/ActivityReducer.js
+++ src/reducers/ActivityReducer.js
@@ -4,7 +4,7 @@ import {
   RESUMED_NON_TIMED_ACTIVITY,
   RESUMED_TIMED_ACTIVITY,
   STARTED_NON_TIMED_ACTIVITY,
-  STARTED_TIMED_ACTIVITY
+  STARTED_TIMED_ACTIVITY, SYNCED_ACTIVITIES
 } from "../events/ActivityEvents";
 import type {Activity} from "../events/ActivityEvents";
 
@@ -52,6 +52,10 @@ const userReducer = (state: ActivityState = INITIAL_ACTIVITY_STATE, action: Acti
       } else {
         state.cache[userGUID] = [activity]
       }
+      return state;
+    }
+    case SYNCED_ACTIVITIES: {
+      state.cache[action.payload] = [];
       return state;
     }
     default:
--- src/sagas/activity/ActivitySyncSaga.js
+++ src/sagas/activity/ActivitySyncSaga.js
@@ -1,11 +1,13 @@
-import {select, call} from 'redux-saga/effects'
+import {select, call, put} from 'redux-saga/effects'
 import {performPost} from "../APISagas";
+import {createSyncedActivitiesEvent} from "../../events/ActivityEvents";
 
 export function* activitySyncSaga() {
   const {user:{information:{guid}}, activity :{cache}} = yield select();
   if(guid && cache[guid]){
     try{
       yield call(performPost, './api/activity/bulk', cache[guid]);
+      yield put(createSyncedActivitiesEvent(guid))
     } catch (e) {
       console.warn(e);
       // todo: handle non-sychage
