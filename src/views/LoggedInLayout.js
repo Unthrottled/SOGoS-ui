11058805aa0af938a9d53ca1bc4b2977e3e643bd
DATBOI <alexsimons9999@gmail.com>
2019-07-03 05:59:15 -0500

    You know, the important stuff.

--- src/views/LoggedInLayout.js
+++ src/views/LoggedInLayout.js
@@ -5,9 +5,18 @@ import SessionExpired from "./SessionExpired";
 import Activity from "./ActivityHub";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
+const pickOne =[
+  'https://acari.io/misc/9eccbbaf0461ecb3c044d582017dd6ee.jpg',
+  'https://static1.e621.net/data/a3/be/a3beb6fd045222a7088e8c886b916ddb.png',
+  'https://acari.io/misc/0ee2189a1b290267840e5cb90ff4a1d8.jpg',
+];
+
 const useStyles = makeStyles(theme =>({
   sharkTits: {
-    background: 'url(https://static1.e621.net/data/a3/be/a3beb6fd045222a7088e8c886b916ddb.png)',
+    background: `url(${pickOne[Math.floor(Math.random() * (pickOne.length + 1))]})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
     height: '100%'
   }
 }));
