0369f44ca8758372631569006a5f6ddcfeaee22a
DATBOI <alexsimons9999@gmail.com>
2019-07-04 07:40:42 -0500

    Logout activity saga test :D

--- src/sagas/activity/LogoutActivitySaga.js
+++ src/sagas/activity/LogoutActivitySaga.js
@@ -1,10 +1,13 @@
 import {createStartedActivityEvent} from "../../events/ActivityEvents";
 import uuid from "uuid/v4";
 import {registerActivitySaga} from "./RegisterActivitySaga";
+import {call} from 'redux-saga/effects'
+
+export const LOGGED_OFF_ACTIVITY_NAME = "LOGGED_OFF";
 
 export function* activityLogoutSaga() {
-  yield registerActivitySaga(createStartedActivityEvent({
-    name: "LOGGED_OFF",
+  yield call(registerActivitySaga, createStartedActivityEvent({
+    name: LOGGED_OFF_ACTIVITY_NAME,
     uuid: uuid()
-  }))
+  }));
 }
