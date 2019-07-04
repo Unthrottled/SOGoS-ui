705da01c8e1ed8da0d60b1bf641031ad01807124
DATBOI <alexsimons9999@gmail.com>
2019-07-04 07:45:53 -0500

    Logon activity saga test :D

--- src/sagas/activity/LogonActivitySaga.js
+++ src/sagas/activity/LogonActivitySaga.js
@@ -2,16 +2,17 @@ import {LOGGED_ON} from "../../events/SecurityEvents";
 import {RECEIVED_USER} from "../../events/UserEvents";
 import {createStartedActivityEvent} from "../../events/ActivityEvents";
 import uuid from "uuid/v4";
-import {all, take} from 'redux-saga/effects'
+import {call, all, take} from 'redux-saga/effects'
 import {registerActivitySaga} from "./RegisterActivitySaga";
 
+export const LOGGED_ON_ACTIVITY_NAME = "LOGGED_ON";
 export function* activityLogonSaga() {
   yield all([
     take(LOGGED_ON),
     take(RECEIVED_USER),
   ]);
-  yield registerActivitySaga(createStartedActivityEvent({
-    name: "LOGGED_ON",
+  yield call(registerActivitySaga, createStartedActivityEvent({
+    name: LOGGED_ON_ACTIVITY_NAME,
     uuid: uuid(),
   }));
 }
