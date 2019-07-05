2431546974a03d9b55d1c12d3adea2744dfda014
DATBOI <alexsimons9999@gmail.com>
2019-07-05 05:09:25 -0500

    Added activity upload saga tests

--- src/__tests__/sagas/activity/RegisterActivitySaga.test.js
+++ src/__tests__/sagas/activity/RegisterActivitySaga.test.js
@@ -1,15 +1,21 @@
-import {call, all, take} from 'redux-saga/effects'
+import {call, put, all, take} from 'redux-saga/effects'
 import sagaHelper from "redux-saga-testing";
 import {activityLogonSaga, LOGGED_ON_ACTIVITY_NAME} from "../../../sagas/activity/LogonActivitySaga";
 import {LOGGED_ON} from "../../../events/SecurityEvents";
 import {RECEIVED_USER} from "../../../events/UserEvents";
 import {
+  ACTIVITY_URL,
   activityCacheSaga,
   activityUploadSaga,
   registerActivitySaga
 } from "../../../sagas/activity/RegisterActivitySaga";
-import {createStartedActivityEvent} from "../../../events/ActivityEvents";
+import {
+  createFailureToRegisterStartEvent,
+  createRegisteredStartEvent,
+  createStartedActivityEvent
+} from "../../../events/ActivityEvents";
 import {isOnline} from "../../../sagas/NetworkSagas";
+import {performPost} from "../../../sagas/APISagas";
 
 
 describe('RegisterActivitySagas', () => {
@@ -43,6 +49,39 @@ describe('RegisterActivitySagas', () => {
         expect(sagaEffect).toEqual(call(
           activityCacheSaga, 'steve'
         ));
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+  });
+  describe('activityUploadSaga', () => {
+    describe('when able to connect with backend', () => {
+      const it = sagaHelper(activityUploadSaga('steve'));
+      it('should talk to the API', sagaEffect => {
+        expect(sagaEffect).toEqual(call(performPost, ACTIVITY_URL, 'steve'));
+      });
+      it('should create expected event', sagaEffect => {
+        expect(sagaEffect).toEqual(put(createRegisteredStartEvent('steve')));
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+    describe('when not able to connect with backend', () => {
+      const it = sagaHelper(activityUploadSaga('steve'));
+      it('should talk to the API', sagaEffect => {
+        expect(sagaEffect).toEqual(call(performPost, ACTIVITY_URL, 'steve'));
+        return new Error('Shit aint gunna work');
+      });
+      it('should create expected event', sagaEffect => {
+        expect(sagaEffect).toEqual(put(createFailureToRegisterStartEvent({
+          error: new Error('Shit aint gunna work'),
+          activity: 'steve'
+        })));
+      });
+      it('should cache activity', sagaEffect => {
+        expect(sagaEffect).toEqual(call(activityCacheSaga, 'steve'));
       });
       it('should complete', sagaEffect => {
         expect(sagaEffect).toBeUndefined();
