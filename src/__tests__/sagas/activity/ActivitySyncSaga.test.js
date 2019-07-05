f1785bbe3763a939f58b0837dfdd859e62124d82
DATBOI <alexsimons9999@gmail.com>
2019-07-05 04:44:00 -0500

    Tested the activity sync saga

---
+++ src/__tests__/sagas/activity/ActivitySyncSaga.test.js
@@ -0,0 +1,127 @@
+import {select, call, put} from 'redux-saga/effects'
+import sagaHelper from "redux-saga-testing";
+import type {UserState} from "../../../reducers/UserReducer";
+import type {ActivityState} from "../../../reducers/ActivityReducer";
+import {activitySyncSaga, BULK_UPLOAD_URL} from "../../../sagas/activity/ActivitySyncSaga";
+import {performPost} from "../../../sagas/APISagas";
+import {createSyncedActivitiesEvent} from "../../../events/ActivityEvents";
+
+describe('ActivitySyncSaga', () => {
+  describe('activitySyncSaga', () => {
+    describe('when user has no global identifier and no cache', () => {
+      const it = sagaHelper(activitySyncSaga());
+      it('should select global state', sagaEffect => {
+        expect(sagaEffect).toEqual(select());
+        const userState: UserState = {
+          information: {}
+        };
+        const activityState: ActivityState = {};
+        return {
+          user: userState,
+          activity: activityState,
+        }
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+    describe('when no cache', () => {
+      const it = sagaHelper(activitySyncSaga());
+      it('should select global state', sagaEffect => {
+        expect(sagaEffect).toEqual(select());
+        const userState: UserState = {
+          information: {
+            guid: 'steve'
+          }
+        };
+        const activityState: ActivityState = {};
+        return {
+          user: userState,
+          activity: activityState,
+        }
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+    describe('when user has nothing cached', () => {
+      const it = sagaHelper(activitySyncSaga());
+      it('should select global state', sagaEffect => {
+        expect(sagaEffect).toEqual(select());
+        const userState: UserState = {
+          information: {
+            guid: 'steve'
+          }
+        };
+        const activityState: ActivityState = {
+          cache: {}
+        };
+        return {
+          user: userState,
+          activity: activityState,
+        }
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+    describe('when user has a cache and shit breaks', () => {
+      const it = sagaHelper(activitySyncSaga());
+      it('should select global state', sagaEffect => {
+        expect(sagaEffect).toEqual(select());
+        const userState: UserState = {
+          information: {
+            guid: 'steve'
+          }
+        };
+        const activityState: ActivityState = {
+          cache: {
+            'steve': 'pants',
+          }
+        };
+        return {
+          user: userState,
+          activity: activityState,
+        }
+      });
+      it('should perform a bulk upload', sagaEffect => {
+        expect(sagaEffect).toEqual(call(performPost, BULK_UPLOAD_URL, 'pants'));
+        return new Error("Shit's broke yo");
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+    describe('when user has a cache and shit works', () => {
+      const it = sagaHelper(activitySyncSaga());
+      it('should select global state', sagaEffect => {
+        expect(sagaEffect).toEqual(select());
+        const userState: UserState = {
+          information: {
+            guid: 'steve'
+          }
+        };
+        const activityState: ActivityState = {
+          cache: {
+            'steve': 'pants',
+          }
+        };
+        return {
+          user: userState,
+          activity: activityState,
+        }
+      });
+      it('should perform a bulk upload', sagaEffect => {
+        expect(sagaEffect).toEqual(call(performPost, BULK_UPLOAD_URL, 'pants'));
+        return {};
+      });
+      it('should create synced activities event', sagaEffect => {
+        expect(sagaEffect).toEqual(put(createSyncedActivitiesEvent('steve')));
+        return {};
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+  });
+});
--- src/sagas/activity/ActivitySyncSaga.js
+++ src/sagas/activity/ActivitySyncSaga.js
@@ -1,12 +1,16 @@
-import {select, call, put} from 'redux-saga/effects'
+import {call, put, select} from 'redux-saga/effects'
 import {performPost} from "../APISagas";
 import {createSyncedActivitiesEvent} from "../../events/ActivityEvents";
+import {selectActivityState, selectUserState} from "../../reducers";
 
+export const BULK_UPLOAD_URL = '/api/activity/bulk';
 export function* activitySyncSaga() {
-  const {user:{information:{guid}}, activity :{cache}} = yield select();
-  if(guid && cache[guid]){
-    try{
-      yield call(performPost, '/api/activity/bulk', cache[guid]);
+  const globalState = yield select();
+  const {information: {guid}} = selectUserState(globalState);
+  const {cache} = selectActivityState(globalState);
+  if (guid && cache && cache[guid]) {
+    try {
+      yield call(performPost, BULK_UPLOAD_URL, cache[guid]);
       yield put(createSyncedActivitiesEvent(guid))
     } catch (e) {
       // todo: handle non-sychage
