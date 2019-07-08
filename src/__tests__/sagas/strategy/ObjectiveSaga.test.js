8a472ead7cc7d4529b7edff7a6bc73af9fd7e817
DATBOI <alexsimons9999@gmail.com>
2019-07-08 05:58:38 -0500

    Added objective fetching tests

---
+++ src/__tests__/sagas/strategy/ObjectiveSaga.test.js
@@ -0,0 +1,33 @@
+import sagaHelper from "redux-saga-testing";
+import {call, put} from 'redux-saga/effects'
+import {objectiveHistoryFetchSaga, OBJECTIVES_URL} from "../../../sagas/strategy/ObjectiveSagas";
+import {performStreamedGet} from "../../../sagas/APISagas";
+import {createFetchedObjectivesEvent} from "../../../events/StrategyEvents";
+
+describe('Objective Sagas', () => {
+  describe('objectiveHistoryFetchSaga', () => {
+    describe('when cannot get data', () => {
+      const it = sagaHelper(objectiveHistoryFetchSaga());
+      it('should try to get objectives', sagaEffect => {
+        expect(sagaEffect).toEqual(call(performStreamedGet, OBJECTIVES_URL));
+        return new Error('shit is broke yo');
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+    describe('when can get data', () => {
+      const it = sagaHelper(objectiveHistoryFetchSaga());
+      it('should try to get objectives', sagaEffect => {
+        expect(sagaEffect).toEqual(call(performStreamedGet, OBJECTIVES_URL));
+        return 'all the data';
+      });
+      it('should let errybody know it got stuff', sagaEffect => {
+        expect(sagaEffect).toEqual(put(createFetchedObjectivesEvent('all the data')))
+      });
+      it('should complete', sagaEffect => {
+        expect(sagaEffect).toBeUndefined();
+      });
+    });
+  });
+});
--- src/sagas/strategy/ObjectiveSagas.js
+++ src/sagas/strategy/ObjectiveSagas.js
@@ -1,10 +1,12 @@
-import {all, call, fork, put, select, take, takeEvery} from 'redux-saga/effects'
+import {call, put} from 'redux-saga/effects'
 import {performStreamedGet} from "../APISagas";
 import {createFetchedObjectivesEvent} from "../../events/StrategyEvents";
 
+export const OBJECTIVES_URL = `/api/strategy/objectives`;
+
 export function* objectiveHistoryFetchSaga() {
   try {
-    const data = yield call(performStreamedGet, `/api/strategy/objectives`);
+    const data = yield call(performStreamedGet, OBJECTIVES_URL);
     yield put(createFetchedObjectivesEvent(data))
   } catch (e) {
     //todo: handle unable to get objectives
