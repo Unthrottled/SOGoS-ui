3d5b641eee24095bb91d45b0af3894ee54ede8a1
DATBOI <alexsimons9999@gmail.com>
2019-07-16 04:24:35 -0500

    Added started activity test

--- src/__tests__/reducers/HistoryReducer.test.js
+++ src/__tests__/reducers/HistoryReducer.test.js
@@ -1,8 +1,38 @@
 import HistoryReducer, {INITIAL_HISTORY_STATE} from "../../reducers/HistoryReducer";
+import {STARTED_ACTIVITY} from "../../events/ActivityEvents";
 
 describe('History Reducer', () => {
   it('should return initial state when given nothing', async () => {
     const historyState = HistoryReducer();
     expect(historyState).toEqual(INITIAL_HISTORY_STATE);
+  });
+  it('should return expected state when given started activity action and initial state', async () => {
+    const action = {
+      type: STARTED_ACTIVITY,
+      payload: 'NEW THING',
+    };
+    const historyState = HistoryReducer(INITIAL_HISTORY_STATE, action);
+    expect(historyState).toEqual({
+      activityFeed: [
+        'NEW THING',
+      ]
+    });
+  });
+  it('should return expected state when given started activity action and some state', async () => {
+    const action = {
+      type: STARTED_ACTIVITY,
+      payload: 'NEW THING',
+    };
+    const historyState = HistoryReducer({
+      activityFeed: [
+        'OLD THING',
+      ]
+    }, action);
+    expect(historyState).toEqual({
+      activityFeed: [
+        'NEW THING',
+        'OLD THING',
+      ]
+    });
   });
 });
