346816d4f965d5cce345561d5abf37bf66fc737c
DATBOI <alexsimons9999@gmail.com>
2019-07-17 04:22:12 -0500

    Added more History Reducer Tests

--- src/__tests__/reducers/HistoryReducer.test.js
+++ src/__tests__/reducers/HistoryReducer.test.js
@@ -1,5 +1,5 @@
 import HistoryReducer, {INITIAL_HISTORY_STATE} from "../../reducers/HistoryReducer";
-import {STARTED_ACTIVITY} from "../../events/ActivityEvents";
+import {RESUMED_NON_TIMED_ACTIVITY, RESUMED_TIMED_ACTIVITY, STARTED_ACTIVITY} from "../../events/ActivityEvents";
 
 describe('History Reducer', () => {
   it('should return initial state when given nothing', async () => {
@@ -21,6 +21,40 @@ describe('History Reducer', () => {
   it('should return expected state when given started activity action and some state', async () => {
     const action = {
       type: STARTED_ACTIVITY,
+      payload: 'NEW THING',
+    };
+    const historyState = HistoryReducer({
+      activityFeed: [
+        'OLD THING',
+      ]
+    }, action);
+    expect(historyState).toEqual({
+      activityFeed: [
+        'NEW THING',
+        'OLD THING',
+      ]
+    });
+  });
+  it('should return expected state when given resumed timed activity action and some state', async () => {
+    const action = {
+      type: RESUMED_TIMED_ACTIVITY,
+      payload: 'NEW THING',
+    };
+    const historyState = HistoryReducer({
+      activityFeed: [
+        'OLD THING',
+      ]
+    }, action);
+    expect(historyState).toEqual({
+      activityFeed: [
+        'NEW THING',
+        'OLD THING',
+      ]
+    });
+  });
+  it('should return expected state when given resumed non timed activity action and some state', async () => {
+    const action = {
+      type: RESUMED_NON_TIMED_ACTIVITY,
       payload: 'NEW THING',
     };
     const historyState = HistoryReducer({
