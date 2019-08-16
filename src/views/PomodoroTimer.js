74dfe63c9dcced841226d3c49aa7e60a78277e05
DATBOI <alexsimons9999@gmail.com>
2019-08-16 05:54:16 -0500

    Y U NO WORK?!

--- src/views/PomodoroTimer.js
+++ src/views/PomodoroTimer.js
@@ -1,24 +1,24 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useRef, useState} from "react";
 import Button from "@material-ui/core/Button";
 import {TimeDisplay} from "./TimeDisplay";
 
 export const PomodoroTimer = ({startTimeInSeconds}) => {
   const [timeElapsed, setTimeElapsed] = useState(0);
-  const [thing, setThing] = useState(9);
+  const [paused, setPaused] = useState(false);
 
   const storedRef = useRef();
-  useEffect(()=>{
-    console.log('here we go again');
-    const emmitter = TimeEmitter(1000, ()=>setThing(thing+1));
-    const disposeable = emmitter((thing)=> {
+
+  if (!(storedRef.current || paused)) {
+    const emmitter = TimeEmitter(1000, () => storedRef.current = undefined);
+    storedRef.current = emmitter((thing) => {
       console.log(timeElapsed + 1);
       setTimeElapsed(timeElapsed + 1)
-    });
-    storedRef.current = disposeable
-  }, [thing]);
+    })
+  }
 
   const pause = () => {
-    storedRef.current()
+    storedRef.current();
+    setPaused(true);
   };
 
 
@@ -29,14 +29,14 @@ export const PomodoroTimer = ({startTimeInSeconds}) => {
             color={'primary'}
             onClick={pause}>Dispose</Button>
   </div>)
-}
+};
 
 
-const TimeEmitter = (timeout=1000, onDispose=()=>{}) => {
+const TimeEmitter = (timeout = 1000, onDispose = () => {}) => {
   let canceled = false;
   return (subscriber) => {
-    const interval = setInterval(()=>{
-      if(!canceled){
+    const interval = setInterval(() => {
+      if (!canceled) {
         subscriber('hola')
       }
     }, timeout);
@@ -47,5 +47,5 @@ const TimeEmitter = (timeout=1000, onDispose=()=>{}) => {
       clearInterval(interval);
       canceled = true;
     }
-  }
-}
+  };
+};
