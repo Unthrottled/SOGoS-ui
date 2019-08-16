d4d738b8834869b742d924b3bb75c4f9c2632b46
DATBOI <alexsimons9999@gmail.com>
2019-08-16 04:55:00 -0500

    Fixo Testo

--- src/__tests__/reducers/NetworkReducer.test.js
+++ src/__tests__/reducers/NetworkReducer.test.js
@@ -4,7 +4,7 @@ import {FOUND_WIFI, LOST_WIFI} from "../../events/NetworkEvents";
 describe('Network Reducer', () => {
   it('should return initial state when given nothing', async () => {
     const networkState = networkReducer();
-    expect(networkState).toEqual({"isOnline": false}
+    expect(networkState).toEqual({"isOnline": false, "hasInternet": false}
     );
   });
   it('should online state when given found WIFI action', async () => {
@@ -12,7 +12,7 @@ describe('Network Reducer', () => {
       type: FOUND_WIFI
     };
     const networkState = networkReducer(INITIAL_NETWORK_STATE, foundWifiAction);
-    expect(networkState).toEqual({"isOnline": true}
+    expect(networkState).toEqual({"isOnline": true, "hasInternet": false}
     );
   });
   it('should online state when given found WIFI action and was offline', async () => {
@@ -20,24 +20,26 @@ describe('Network Reducer', () => {
       type: FOUND_WIFI
     };
     const networkState = networkReducer({
+      ...INITIAL_NETWORK_STATE,
       isOnline: false,
     }, foundWifiAction);
-    expect(networkState).toEqual({"isOnline": true});
+    expect(networkState).toEqual({"isOnline": true, "hasInternet": false});
   });
   it('should Offline state when given found WIFI action', async () => {
     const foundWifiAction = {
       type: LOST_WIFI
     };
     const networkState = networkReducer(INITIAL_NETWORK_STATE, foundWifiAction);
-    expect(networkState).toEqual({"isOnline": false});
+    expect(networkState).toEqual({"isOnline": false, "hasInternet": false});
   });
   it('should Offline state when given found WIFI action and was online', async () => {
     const foundWifiAction = {
       type: LOST_WIFI
     };
     const networkState = networkReducer({
+      ...INITIAL_NETWORK_STATE,
       isOnline: true,
     }, foundWifiAction);
-    expect(networkState).toEqual({"isOnline": false});
+    expect(networkState).toEqual({"isOnline": false, "hasInternet": false});
   });
 });
