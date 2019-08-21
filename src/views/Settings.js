28ca0363af3ee3eb1c6028ca21ded61de7acfd46
DATBOI <alexsimons9999@gmail.com>
2019-08-21 05:04:31 -0500

    Savey Settings, now just need to load

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -7,6 +7,7 @@ import SaveIcon from "@material-ui/icons/Save";
 import Fab from "@material-ui/core/Fab";
 import {Typography} from "@material-ui/core";
 import {withRouter} from "react-router-dom";
+import {createUpdatedPomodoroSettingsEvent} from "../events/TacticalEvents";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -54,6 +55,33 @@ const restMarks = [
   },
 ];
 
+const recoveryMarks = [
+  {
+    value: 0,
+    label: '0',
+  },
+  {
+    value: 5,
+    label: '5',
+  },
+  {
+    value: 10,
+    label: '10',
+  },
+  {
+    value: 20,
+    label: '20',
+  },
+  {
+    value: 30,
+    label: '30',
+  },
+  {
+    value: 60,
+    label: '60',
+  },
+];
+
 const workMarks = [
   {
     value: 0,
@@ -83,22 +111,32 @@ const SettingsBoard = ({
                          pomodoroSettings,
                        }) => {
   const classes = useStyles();
-  const saveSettings = () => {
-
-    // history.push('/')
-  };
-
   const [recoveryTime, setRecoveryTime] = useState(5);
+
   const saveRecoveryTime = (_, time) => {
     setRecoveryTime(time)
   };
+  const [longRecoveryTime, setLongRecoveryTime] = useState(30);
 
+  const saveLongRecoveryTime = (_, time) => {
+    setLongRecoveryTime(time)
+  };
   const [workTime, setWorkTime] = useState(25);
+
   const saveWorkTime = (_, time) => {
     setWorkTime(time)
   };
-
   const cycleTimeMinutes = (workTime + recoveryTime) * 4 - recoveryTime;
+
+  const saveSettings = () => {
+    dispatch(createUpdatedPomodoroSettingsEvent({
+      loadDuration: workTime,
+      shortRecoveryDuration: recoveryTime,
+      longRecoveryDuration: longRecoveryTime,
+    }))
+    // history.push('/')
+  };
+
   return (
     <LoggedInLayout>
       <div className={classes.container}>
@@ -106,27 +144,37 @@ const SettingsBoard = ({
           id={'pomodoro-work-time'}
           label={'Working Duration (minutes)'}
           defaultValue={workTime}
-          helperText={'How long to work before a break.'}
           aria-labelledby="discrete-slider-always"
           step={0.5}
           onChangeCommitted={saveWorkTime}
           min={5}
           marks={workMarks}
           max={workMarks[workMarks.length - 1].value}
-          valueLabelDisplay="on"
+          valueLabelDisplay="auto"
         />
         <Slider
           id={'pomodoro-recovery-time'}
           label={'Working Duration (minutes)'}
           defaultValue={recoveryTime}
-          helperText={'How long to recover before work.'}
           aria-labelledby="discrete-slider-always"
           step={0.5}
           onChangeCommitted={saveRecoveryTime}
           min={0.5}
           marks={restMarks}
           max={restMarks[restMarks.length - 1].value}
-          valueLabelDisplay="on"
+          valueLabelDisplay="auto"
+        />
+        <Slider
+          id={'pomodoro-long-recovery-time'}
+          label={'Working Duration (minutes)'}
+          defaultValue={longRecoveryTime}
+          aria-labelledby="discrete-slider-always"
+          step={0.5}
+          onChangeCommitted={saveLongRecoveryTime}
+          min={5}
+          marks={recoveryMarks}
+          max={recoveryMarks[recoveryMarks.length - 1].value}
+          valueLabelDisplay="auto"
         />
         <div>
           <Typography>
