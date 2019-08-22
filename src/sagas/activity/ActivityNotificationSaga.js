9f543066fc3bbe2c87ad1efb03e2d5dca2183a19
DATBOI <alexsimons9999@gmail.com>
2019-08-22 05:54:57 -0500

    Neat! Sound!

--- src/sagas/activity/ActivityNotificationSaga.js
+++ src/sagas/activity/ActivityNotificationSaga.js
@@ -2,10 +2,13 @@ import {select} from 'redux-saga/effects'
 import {selectConfigurationState} from "../../reducers";
 
 
+//todo: only notify after starting.
 export function* activityNotificationSaga() {
   const {miscellaneous: {notificationsAllowed}} = yield select(selectConfigurationState);
   if (notificationsAllowed === "granted" && ("Notification" in window)) {
     new Notification("Fam ravioli");
-    window.navigator.vibrate([200, 100, 200])
+    window.navigator.vibrate([200, 100, 200]);
+    const audio = new Audio('/notif.mp3');
+    audio.play().then(()=>{})
   }
 }
