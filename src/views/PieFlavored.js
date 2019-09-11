3b8e90a9d2d1a24b52bb0a4c197fc92d3ddba186
DATBOI <alexsimons9999@gmail.com>
2019-09-11 05:14:59 -0500

    Less drawing when no activity feed

--- src/views/PieFlavored.js
+++ src/views/PieFlavored.js
@@ -68,56 +68,58 @@ const PieFlavored = ({activityFeed}) => {
     }, []);
 
   useEffect(() => {
-    const selection = select('#pieBoi');
-    const width = 200;
-    const height = 200;
+    if(activityFeed.length > 0){
+      const selection = select('#pieBoi');
+      const width = 200;
+      const height = 200;
 
-    selection.select('svg').remove();
+      selection.select('svg').remove();
 
-    const pieSVG = selection.append('svg')
-      .attr("viewBox", [-width / 2, -height / 2, width, height])
-      .style("height", '100%');
+      const pieSVG = selection.append('svg')
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .style("height", '100%');
 
-    const pieFlavored = pie()
-      .padAngle(0.005)
-      .sort(null)
-      .value(d => d.value);
+      const pieFlavored = pie()
+        .padAngle(0.005)
+        .sort(null)
+        .value(d => d.value);
 
-    const arcs = pieFlavored(pieData);
-    const color = scaleOrdinal()
-      .domain(pieData.map(d => d.value))
-      .range(quantize(t => interpolateSpectral(t * 0.8 + 0.1), pieData.length).reverse());
+      const arcs = pieFlavored(pieData);
+      const color = scaleOrdinal()
+        .domain(pieData.map(d => d.value))
+        .range(quantize(t => interpolateSpectral(t * 0.8 + 0.1), pieData.length).reverse());
 
-    const radius = Math.min(width, height) / 2;
-    const arcThing = arc()
-      .innerRadius(radius * 0.67)
-      .outerRadius(radius - 1);
+      const radius = Math.min(width, height) / 2;
+      const arcThing = arc()
+        .innerRadius(radius * 0.67)
+        .outerRadius(radius - 1);
 
-    pieSVG.selectAll("path")
-      .data(arcs)
-      .join("path")
-      .attr("fill", d => color(d.data.value))
-      .attr("d", arcThing)
-      .append("title")
-      .text(d => `${d.data.name}: ${d.data.value.toLocaleString()}`);
+      pieSVG.selectAll("path")
+        .data(arcs)
+        .join("path")
+        .attr("fill", d => color(d.data.value))
+        .attr("d", arcThing)
+        .append("title")
+        .text(d => `${d.data.name}: ${d.data.value.toLocaleString()}`);
 
-    pieSVG.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 12)
-      .attr("text-anchor", "middle")
-      .selectAll("text")
-      .data(arcs)
-      .join("text")
-      .attr("transform", d => `translate(${arcThing.centroid(d)})`)
-      .call(text => text.append("tspan")
-        .attr("y", "-0.4em")
-        .attr("font-weight", "bold")
-        .text(d => d.data.name))
-      .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
-        .attr("x", 0)
-        .attr("y", "0.7em")
-        .attr("fill-opacity", 0.7)
-        .text(d => d.data.value.toLocaleString()));
+      pieSVG.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 12)
+        .attr("text-anchor", "middle")
+        .selectAll("text")
+        .data(arcs)
+        .join("text")
+        .attr("transform", d => `translate(${arcThing.centroid(d)})`)
+        .call(text => text.append("tspan")
+          .attr("y", "-0.4em")
+          .attr("font-weight", "bold")
+          .text(d => d.data.name))
+        .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
+          .attr("x", 0)
+          .attr("y", "0.7em")
+          .attr("fill-opacity", 0.7)
+          .text(d => d.data.value.toLocaleString()));
+    }
   });
 
   return (
