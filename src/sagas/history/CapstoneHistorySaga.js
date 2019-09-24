a88a2e71b971338fea49250c4a02a8f0cc7c1c42
DATBOI <alexsimons9999@gmail.com>
2019-09-24 05:06:20 -0500

    More able to get the after capstone

--- src/sagas/history/CapstoneHistorySaga.js
+++ src/sagas/history/CapstoneHistorySaga.js
@@ -27,7 +27,6 @@ export function* getFirstBefore(selectedFromDate: number,
     (activity: Activity) => activity.antecedenceTime - selectedFromDate);
   if (activityIndex < 0 && (activityIndex === -1 || activityIndex === -(activityIndex.length + 1))) {
     console.log("great I gotta talk to the API, with the from date as a parameter");
-    // Take oldest item
     const oldestTime = activities.length ? activities[activities.length - 1].antecedenceTime : from;
     const {data} = yield call(findFirstActivityBeforeTime, oldestTime);
     if(data) {
@@ -38,30 +37,42 @@ export function* getFirstBefore(selectedFromDate: number,
   return activities[activities.length - 1];
 }
 
-export const constructActivityBeforeURL = (guid: string) => `/api/history/${guid}/first/before`;
-
-export const constructActivityAfterURL = (guid: string) => `/api/history/${guid}/first/after`;
-
-export function* findFirstActivityBeforeTime(relativeTime: number): Activity {
-  const {information: {guid}}: UserState = yield select(selectUserState);
-  const beforeURL = constructActivityBeforeURL(guid);
-  try {
-    return yield call(performPost, beforeURL, {relativeTime});
-  } catch (e) {
-    return undefined
-  }
-}
-
 export function* getFirstAfter(selectedToRange: number,
                                {activities, timeRange: {from, to}}: FullRangeAndFeed) {
   const activityIndex = reverseBinarySearch(activities,
     (activity: Activity) => activity.antecedenceTime - selectedToRange);
   if (activityIndex < 0 && (activityIndex === -1 || activityIndex === -(activityIndex.length + 1))) {
     console.log("great I gotta talk to the API, with the to date as a parameter");
-    // Take youngest item
-    // Get First after
-  } else {
-    console.log("Looks Like I can do stuff with what I got");
+    const youngestTime = activities.length ? activities[0].antecedenceTime : to;
+    const {data} = yield call(findFirstActivityAfterTime, youngestTime);
+    if(data) {
+      return data
+    }
+  }
+
+  console.log("Looks Like I can do stuff with what I got");
+  return activities[0];
+}
+
+export const constructActivityBeforeURL = (guid: string) => `/api/history/${guid}/first/before`;
+
+export const constructActivityAfterURL = (guid: string) => `/api/history/${guid}/first/after`;
+
+export function* findFirstActivityBeforeTime(relativeTime: number): Activity {
+  return yield call(findCapstoneActivity, relativeTime, constructActivityBeforeURL)
+}
+
+export function* findFirstActivityAfterTime(relativeTime: number): Activity {
+  return yield call(findCapstoneActivity, relativeTime, constructActivityAfterURL)
+}
+
+export function* findCapstoneActivity(relativeTime: number, urlBuilder: (string) => string) : Activity {
+  const {information: {guid}}: UserState = yield select(selectUserState);
+  const beforeURL = urlBuilder(guid);
+  try {
+    return yield call(performPost, beforeURL, {relativeTime});
+  } catch (e) {
+    return {}
   }
 }
 
