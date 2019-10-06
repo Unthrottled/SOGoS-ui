4b3808d7d77b201e0e50fcbf6063fb9b59961500
DATBOI <alexsimons9999@gmail.com>
2019-10-06 11:07:09 -0500

    I think I will need this

--- src/views/StrategicDashboard.js
+++ src/views/StrategicDashboard.js
@@ -7,11 +7,16 @@ import {Card, makeStyles} from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import {GoalIcon} from "./GoalIcon";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: 345,
     margin: 'auto',
+  },
+  headerContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
   },
   goalIcon: {
     padding: theme.spacing(2)
@@ -22,6 +27,22 @@ const StrategicDashboard = ({history}) => {
   const classes = useStyles();
   return (
     <LoggedInLayout>
+      <div className={classes.headerContent}>
+        <Container maxWidth={'sm'}>
+          <Typography component={'h1'}
+                      variant={'h2'}
+                      align={'center'}
+                      color={'textPrimary'}
+                      gutterBottom>
+            Command Center
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            Something short and leading about the collection below—its contents, the creator, etc.
+            Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+            entirely.
+          </Typography>
+        </Container>
+      </div>
       <Card className={classes.card}>
         <CardActionArea onClick={() => history.push('./objectives/')}>
           <div className={classes.goalIcon}>
