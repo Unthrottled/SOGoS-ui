2604f0aec9329b9867fd1eadea5de516fe0f0a26
DATBOI <alexsimons9999@gmail.com>
2019-11-12 06:07:05 -0600

    Well that was fast

--- SOGoS-Next-Gen/src/reducers/MiscReducer.ts
+++ SOGoS-Next-Gen/src/reducers/MiscReducer.ts
@@ -1,45 +1,44 @@
-import {Action} from "redux";
 import {DISMISSED_NOTIFICATION, REQUESTED_NOTIFICATION} from "../events/MiscEvents";
 
-export type NotificationState = {
-  message: string,
-  shown: boolean,
-  type: string,
+export interface NotificationState {
+    message: string;
+    shown: boolean;
+    type: string;
 }
 
-export type MiscState = {
-  notification: NotificationState,
+export interface MiscState {
+    notification: NotificationState;
 }
 
 export const INITIAL_MISC_STATE: MiscState = {
-  notification: {
-    message: 'We done goofed.',
-    shown: false,
-    type: 'warning'
-  }
+    notification: {
+        message: 'We done goofed.',
+        shown: false,
+        type: 'warning'
+    }
 };
 
-const MiscReducer = (state: MiscState = INITIAL_MISC_STATE, action: Action = {}) => {
-  switch (action.type) {
-    case DISMISSED_NOTIFICATION:
-      return {
-        ...state,
-        notification:{
-          ...state.notification,
-          shown: false
-        },
-      };
-    case REQUESTED_NOTIFICATION:
-      return {
-        ...state,
-        notification:{
-          message: action.payload,
-          shown: true
-        },
-      };
-    default:
-      return state
-  }
+const MiscReducer = (state: MiscState = INITIAL_MISC_STATE, action: any) => {
+    switch (action.type) {
+        case DISMISSED_NOTIFICATION:
+            return {
+                ...state,
+                notification: {
+                    ...state.notification,
+                    shown: false
+                },
+            };
+        case REQUESTED_NOTIFICATION:
+            return {
+                ...state,
+                notification: {
+                    message: action.payload,
+                    shown: true
+                },
+            };
+        default:
+            return state
+    }
 };
 
 export default MiscReducer;
