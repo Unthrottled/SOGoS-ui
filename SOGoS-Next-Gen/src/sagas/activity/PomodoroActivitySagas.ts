475a2cddd619843cfeabd044c5c7cd5ded625995
DATBOI <alexsimons9999@gmail.com>
2019-11-13 05:39:42 -0600

    Pomodoro conversion

--- SOGoS-Next-Gen/src/sagas/activity/PomodoroActivitySagas.ts
+++ SOGoS-Next-Gen/src/sagas/activity/PomodoroActivitySagas.ts
@@ -1,6 +1,6 @@
 import {selectActivityState} from "../../reducers";
 import {put, select, take} from 'redux-saga/effects'
-import type {ActivityState} from "../../reducers/ActivityReducer";
+import {ActivityState} from "../../reducers/ActivityReducer";
 import {createInitializedPomodoroEvent} from "../../events/ActivityEvents";
 import {INITIALIZED_APPLICATION} from "../../events/ApplicationLifecycleEvents";
 
@@ -12,7 +12,7 @@ export function* pomodoroActivityInitializationSaga() {
   const meow = new Date().valueOf();
   const todaysDay = (meow / ONE_DAY);
   const rememberedDay = Math.floor((completedPomodoro.dateCounted / ONE_DAY));
-  if(todaysDay !== rememberedDay){
+  if (todaysDay !== rememberedDay) {
     yield put(createInitializedPomodoroEvent({
       dateCounted: meow,
       count: 0,
