c7d37de00692529c319bb587295b5cc95d2c29de
DATBOI <alexsimons9999@gmail.com>
2019-11-13 05:42:30 -0600

    EZ4ENCE

--- SOGoS-Next-Gen/src/sagas/configuration/ConfigurationConvienenceSagas.ts
+++ SOGoS-Next-Gen/src/sagas/configuration/ConfigurationConvienenceSagas.ts
@@ -1,18 +1,18 @@
 import {put, take} from 'redux-saga/effects'
 import {
-  createRequestForInitialConfigurations, FOUND_INITIAL_CONFIGURATION,
+  createRequestForInitialConfigurations,
+  FOUND_INITIAL_CONFIGURATION,
   RECEIVED_OAUTH_CONFIGURATION,
   requestOAuthConfigurations
 } from "../../events/ConfigurationEvents";
-import type {InitialConfig, OAuthConfig} from "../../types/ConfigurationModels";
 
-export function* oauthConfigurationSaga(): OAuthConfig {
+export function* oauthConfigurationSaga() {
   yield put(requestOAuthConfigurations()); // ask for oauth configurations
   const {payload: oauthConfig} = yield take(RECEIVED_OAUTH_CONFIGURATION); // yay configurations!
   return oauthConfig;
 }
 
-export function* initialConfigurationSaga(): InitialConfig {
+export function* initialConfigurationSaga() {
   yield put(createRequestForInitialConfigurations());
   const {payload: initialConfigurations} = yield take(FOUND_INITIAL_CONFIGURATION);
   return initialConfigurations
