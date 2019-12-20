0c93c807f65e4a0389927ce16215d71b56539244
DATBOI <alexsimons9999@gmail.com>
2019-12-20 07:37:16 -0600

    I keep forget about this

--- SOGoS-UI/src/sagas/configuration/InitialConfigurationSagas.ts
+++ SOGoS-UI/src/sagas/configuration/InitialConfigurationSagas.ts
@@ -20,7 +20,7 @@ export function* initialConfigurationSaga() {
     const {data: configData} = yield call(performFullOpenGet, '/config/initial.json');
     const {data: {currentTime}} = yield call(performFullOpenGet, `${configData.apiURL}/time`);
     const meow = new Date().valueOf();
-    if (meow - currentTime < 10000) {
+    if (meow - currentTime > 10000) {
       yield put(createOutOfSyncEvent());
     } else {
       yield put(createReceivedPartialInitialConfigurationsEvent(configData));
