93190f23881ce8b11b2eb3f4eb1fe7ba7479f617
DATBOI <alexsimons9999@gmail.com>
2020-01-07 17:57:56 -0600

    Less streaming :(

--- SOGoS-UI/src/sagas/APISagas.ts
+++ SOGoS-UI/src/sagas/APISagas.ts
@@ -1,66 +1,15 @@
-import {call, select, take} from 'redux-saga/effects'
-import {buffers, END, eventChannel} from 'redux-saga';
+import {call, select} from 'redux-saga/effects'
 import axios from 'axios';
-import oboe from 'oboe';
 import {accessTokenWithoutSessionExtensionSaga, accessTokenWithSessionExtensionSaga} from "./security/AccessTokenSagas";
 import {selectConfigurationState} from "../reducers";
 import {ConfigurationState} from "../reducers/ConfigurationReducer";
 
-const SHITS_BROKE_YO: string = "SHIT'S BROKE YO";
-
-type ChannelParameters = { url: string, method: string, headers: any, body: any };
-export const createStreamChannel = ({url, method, headers, body}: ChannelParameters) => {
-  return eventChannel(statusObserver => {
-    const requestStream = oboe({
-      url,
-      method,
-      headers,
-      body,
-      'cached': false,
-      'withCredentials': true
-    }).done((jsonThingo: any) => {
-      statusObserver(jsonThingo);
-    }).fail((error: any) => {
-      console.log('Error streaming', error);
-      statusObserver(SHITS_BROKE_YO);
-      statusObserver(END);
-    }).on('end', () => {
-      statusObserver(END);
-    });
-    return () => requestStream.abort();
-  }, buffers.expanding(100))
-};
-
-export function* performStreamedGet<T>(url: String, options = {headers: {}}) {
-  const headers = yield call(createHeaders, accessTokenWithSessionExtensionSaga, options);
-  const fullURL = yield call(constructURL, url);
-  const streamChannel = yield call(createStreamChannel, {
-    url: fullURL,
-    method: 'GET',
-    headers,
-    body: '',
-  });
-  const aggregate = [];
-  let error;
-  try {
-    while (true) {
-      const itemChunk = yield take(streamChannel);
-      if(itemChunk !== SHITS_BROKE_YO) {
-        aggregate.unshift(itemChunk);
-      } else {
-        error = itemChunk;
-      }
-    }
-  } finally {
-    if(!error){
-      return aggregate; //dis dumb
-    } else {
-      throw new Error('yeet');
-    }
-  }
+export function* performStreamedGet<T>(url: string, options = {headers: {}}) {
+  const result = yield call(performGet, url, options);
+  return result.data;
 }
 
-export function* createHeaders(accessTokenSaga: ()=>any, options = {headers: {}}) {
+export function* createHeaders(accessTokenSaga: () => any, options = {headers: {}}) {
   const accessToken = yield call(accessTokenSaga);
   const {user: {information: {guid}}, security: {verificationKey}} = yield select();
   return {
@@ -72,13 +21,13 @@ export function* createHeaders(accessTokenSaga: ()=>any, options = {headers: {}}
 }
 
 export function* constructURL(url: String) {
-  const { initial: {apiURL}}: ConfigurationState = yield select(selectConfigurationState);
+  const {initial: {apiURL}}: ConfigurationState = yield select(selectConfigurationState);
   return `${apiURL || ''}${url}`
 }
 
 export function* performGetWithToken(url: string,
                                      options: any,
-                                     accessTokenSaga: ()=>any) {
+                                     accessTokenSaga: () => any) {
   const headers = yield call(createHeaders, accessTokenSaga, options);
   const fullURL = yield call(constructURL, url);
   return yield call(axios.get, fullURL, {
@@ -88,7 +37,7 @@ export function* performGetWithToken(url: string,
 }
 
 export function* performGet(url: string,
-                               options = {headers: {}}) {
+                            options = {headers: {}}) {
   return yield call(performGetWithToken, url, options, accessTokenWithSessionExtensionSaga);
 }
 
