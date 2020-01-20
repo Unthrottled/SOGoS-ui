a80e4d9a24ba7af36637314de02b9694a604231a
DATBOI <alexsimons9999@gmail.com>
2020-01-20 06:02:30 -0600

    Will need to revisit the refresh logik

--- SOGoS-UI/src/config/ReactotronConfiguration.ts
+++ SOGoS-UI/src/config/ReactotronConfiguration.ts
@@ -1,6 +1,15 @@
 import Reactotron from 'reactotron-react-js';
 import {reactotronRedux} from 'reactotron-redux';
 
+declare global {
+  interface Console {
+    tron: (...args: any[]) => void;
+  }
+}
+
+// @ts-ignore
+console.tron = Reactotron.log;
+
 const reactotronInstance = Reactotron.configure({
   name: 'SOGoS-UI',
 })
