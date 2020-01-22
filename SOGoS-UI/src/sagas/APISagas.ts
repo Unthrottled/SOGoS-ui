347a96716dc68b56f667a4aa07d234f1c48e592a
DATBOI <alexsimons9999@gmail.com>
2020-01-22 04:51:19 -0600

    Putting this back because I am sticking with old version

--- SOGoS-UI/src/sagas/APISagas.ts
+++ SOGoS-UI/src/sagas/APISagas.ts
@@ -1,4 +1,6 @@
 import {call, delay, race, select, take} from 'redux-saga/effects';
+import {buffers, END, eventChannel} from 'redux-saga';
+import oboe from "oboe";
 import axios from 'axios';
 import {accessTokenWithSessionExtensionSaga,} from './security/AccessTokenSagas';
 import {selectConfigurationState} from '../reducers';
@@ -9,9 +11,58 @@ import {RECEIVED_USER} from '../events/UserEvents';
 import {RECEIVED_PARTIAL_INITIAL_CONFIGURATION} from '../events/ConfigurationEvents';
 import {InitialConfig} from '../types/ConfigurationTypes';
 
-export function* performStreamedGet<T>(url: string, options = {headers: {}}) {
-  const result = yield call(performGet, url, options);
-  return result.data;
+const SHITS_BROKE_YO: string = "SHIT'S BROKE YO";
+
+type ChannelParameters = { url: string, method: string, headers: any, body: any };
+export const createStreamChannel = ({url, method, headers, body}: ChannelParameters) => {
+  return eventChannel(statusObserver => {
+    const requestStream = oboe({
+      url,
+      method,
+      headers,
+      body,
+      'cached': false,
+      'withCredentials': true
+    }).done((jsonThingo: any) => {
+      statusObserver(jsonThingo);
+    }).fail((error: any) => {
+      console.log('Error streaming', error);
+      statusObserver(SHITS_BROKE_YO);
+      statusObserver(END);
+    }).on('end', () => {
+      statusObserver(END);
+    });
+    return () => requestStream.abort();
+  }, buffers.expanding(100))
+};
+
+export function* performStreamedGet<T>(url: String, options = {headers: {}}) {
+  const headers = yield call(createHeaders, accessTokenWithSessionExtensionSaga, options);
+  const fullURL = yield call(constructURL, url);
+  const streamChannel = yield call(createStreamChannel, {
+    url: fullURL,
+    method: 'GET',
+    headers,
+    body: '',
+  });
+  const aggregate = [];
+  let error;
+  try {
+    while (true) {
+      const itemChunk = yield take(streamChannel);
+      if(itemChunk !== SHITS_BROKE_YO) {
+        aggregate.unshift(itemChunk);
+      } else {
+        error = itemChunk;
+      }
+    }
+  } finally {
+    if(!error){
+      return aggregate; //dis dumb
+    } else {
+      throw new Error('yeet');
+    }
+  }
 }
 
 function* getAuthorizationStuff() {
