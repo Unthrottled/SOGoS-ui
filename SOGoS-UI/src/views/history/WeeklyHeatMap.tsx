49ad64dc92e52f0cfba3f2802566f72612f50f37
DATBOI <alexsimons9999@gmail.com>
2020-02-17 10:17:42 -0600

    Legend27

--- SOGoS-UI/src/views/history/WeeklyHeatMap.tsx
+++ SOGoS-UI/src/views/history/WeeklyHeatMap.tsx
@@ -1,4 +1,5 @@
 import React, {FC, useEffect, useState} from 'react';
+import range from 'lodash/range';
 import {connect} from 'react-redux';
 import {max, scaleSequential, select} from 'd3';
 import {
@@ -23,6 +24,19 @@ import {MenuItem, Select} from '@material-ui/core';
 import {mapTacticalActivitiesToID} from './PieFlavored';
 import {ActivityProjection} from './Projections';
 import {constructColorMappings} from './TimeLine';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import {TacticalActivityIcon} from '../icons/TacticalActivityIcon';
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    borderTop: 'solid 1px rgba(0,0,0, .15)',
+  },
+  legendLabel: {
+    flexGrow: 1,
+    textAlign: 'right',
+  },
+}));
 
 interface Props {
   activityFeed: Activity[];
@@ -277,6 +291,34 @@ const WeeklyHeatMap: FC<Props> = ({
       .attr('height', gridSize)
       .style('opacity', d => opacityScale(d.value))
       .style('fill', d => colorScale(d.value));
+
+    const legend = select('#legend27');
+    legend.select('svg').remove();
+    const legendRange = range(0, 1, 0.1);
+    const legendWidth = (legendRange.length) * gridSize;
+    const legendSvg = legend
+      .append('svg')
+      .attr('width', legendWidth)
+      .attr('height', gridSize);
+
+    const legendRect = legendSvg.selectAll('.legend').data(legendRange);
+
+    legendRect
+      .enter()
+      .append('g')
+      .attr('class', 'legend')
+      .append('rect')
+      .attr('x', (d, i) => gridSize * i)
+      .style('fill', (d, i) => colorScale(i))
+      .attr('rx', 4)
+      .attr('ry', 4)
+      .attr('class', 'hour bordered')
+      .attr('width', gridSize)
+      .attr('height', gridSize)
+      .style('opacity', d => opacityScale(d))
+      .style('fill', d => colorScale(d));
+
+    legendRect.exit().remove();
   }, [
     activityFeed,
     archivedActivities,
@@ -323,28 +365,50 @@ const WeeklyHeatMap: FC<Props> = ({
       setCurrentActivity(activityOptions[0]);
     }
   }, [activityOptions, currentActivity]);
-
+  const mappedTacticalActivities = mapTacticalActivitiesToID(
+    tacticalActivities,
+  );
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.container}>
       <div id={'heatBoi'} />
-      <Select
-        value={currentActivity.name}
-        onChange={event => {
-          const nextSelection = activityOptions.find(
-            option => option.name === event.target.value,
-          );
-          setCurrentActivity(nextSelection || activityOptions[0]);
-        }}>
-        {activityOptions.map(value => {
-          return (
-            <MenuItem
-              key={value.key || new Date().valueOf().toString(16)}
-              value={value.name}>
-              {value.name}
-            </MenuItem>
-          );
-        })}
-      </Select>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <div style={{margin: 'auto 0'}}>
+          <TacticalActivityIcon
+            size={{
+              width: 50,
+              height: 50,
+            }}
+            tacticalActivity={mappedTacticalActivities[currentActivity.key]}
+          />
+        </div>
+        <Select
+          style={{marginLeft: '0.5rem'}}
+          value={currentActivity.name}
+          onChange={event => {
+            const nextSelection = activityOptions.find(
+              option => option.name === event.target.value,
+            );
+            setCurrentActivity(nextSelection || activityOptions[0]);
+          }}>
+          {activityOptions.map(value => {
+            return (
+              <MenuItem
+                key={value.key || new Date().valueOf().toString(16)}
+                value={value.name}>
+                {value.name}
+              </MenuItem>
+            );
+          })}
+        </Select>
+        <div>
+          <div id={'legend27'} />
+          <div style={{display: 'flex'}}>
+            <Typography>less</Typography>
+            <Typography className={classes.legendLabel}>more</Typography>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -369,5 +433,3 @@ const mapStateToProps = (state: GlobalState): Props => {
 };
 
 export default connect(mapStateToProps)(WeeklyHeatMap);
-
-// heatmap will show one/more activity.
