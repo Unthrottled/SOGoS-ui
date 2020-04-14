e2de2be0aaba75ef17ccf80cb581f6d85cf1ca12
DATBOI <alexsimons9999@gmail.com>
2020-04-14 05:34:06 -0500

    Less lint

--- src/sagas/activity/LogoutActivitySaga.ts
+++ src/sagas/activity/LogoutActivitySaga.ts
@@ -1,10 +1,2 @@
-import {createStartedActivityEvent} from '../../events/ActivityEvents';
-import uuid from 'uuid/v4';
-import {registerActivitySaga} from './RegisterActivitySaga';
-import {call} from 'redux-saga/effects';
-import {ActivityTimedType, ActivityType} from '../../types/ActivityTypes';
-
-export const LOGGED_OFF_ACTIVITY_NAME = 'LOGGED_OFF';
-
 export function* activityLogoutSaga() {
 }
